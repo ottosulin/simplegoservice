fb64ada0571ce7253ce5b30f8c345268f1cfabb1
Otto Sulin <otto.sulin@gmail.com>
2022-05-20 21:22:06 +0300

    Add error handling

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	httpserver "github.com/ottosulin/simplegoservice/pkg"
@@ -10,5 +11,8 @@ func main() {
 	http.HandleFunc("/hello", httpserver.Hello)
 	http.HandleFunc("/headers", httpserver.Headers)
 
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalln("Error happened: ", err)
+	}
 }
